74f278daac0be0bfadcd1747877ac25dbb99cf76
Sams Uddin Ahamed <thesua711@gmail.com>
2019-04-23 01:39:20 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+        var express = require('express');
+        var bodyParser = require('body-parser');
+        var path = require('path');
+
+
+        var app = express();
+
+        app.listen(3000,function(){
+            console.log('Server started on port 3000.. ');
+        })
+
+        app.get('/',function(req,res){
+            res.sendFile(__dirname+'/index.html'); 
+            
+            // app.get('path',function(request,response){}) --Structure of GET
+            //$ ./node_modules/.bin/nodemon server.js to run nodemon
+
+
+        })
